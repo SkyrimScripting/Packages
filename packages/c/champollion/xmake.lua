d68fecee25dd702383bd83d00c37094d3ef06d29
Mrowr Purr <mrowr.purr@gmail.com>
2023-10-18 14:05:08 -0700

    CMAKE_BUILD_TYPE

--- packages/c/champollion/xmake.lua
+++ packages/c/champollion/xmake.lua
@@ -1,7 +1,17 @@
+add_rules("mode.debug", "mode.release")
+
 package("champollion")
     set_homepage("https://github.com/Orvid/Champollion")
     set_description("A description for Champollion")
     add_urls("https://github.com/Orvid/Champollion.git")
     on_install(function(package)
-        import("package.tools.cmake").install(package, {"-DCHAMPOLLION_STATIC_LIBRARY=ON"})
+        local configs = {"-DCHAMPOLLION_STATIC_LIBRARY=ON"}
+    
+        if package:debug() then
+            table.insert(configs, "-DCMAKE_BUILD_TYPE=Debug")
+        else
+            table.insert(configs, "-DCMAKE_BUILD_TYPE=Release")
+        end
+
+        import("package.tools.cmake").install(package, configs)
     end)
