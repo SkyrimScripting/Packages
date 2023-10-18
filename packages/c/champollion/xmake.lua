cfe0f68f6ac62cc3c5fe4d75679fc2f357ea3165
Mrowr Purr <mrowr.purr@gmail.com>
2023-10-18 12:49:00 -0700

    Add champollion

---
+++ packages/c/champollion/xmake.lua
@@ -0,0 +1,27 @@
+package("champollion")
+    set_homepage("https://github.com/Orvid/Champollion")
+    set_description("A description for Champollion")
+    add_urls("https://github.com/Orvid/Champollion")
+    
+    add_configs("standalone", { description = "Compile as a static library.", default = false, type = "boolean"})
+
+    on_install(function(package)
+        local configs = {}
+        if package:config("standalone") then
+            table.insert(configs, "-DCHAMPOLLION_STATIC_LIBRARY=ON")
+        else
+            table.insert(configs, "-DCHAMPOLLION_STATIC_LIBRARY=OFF")
+        end
+        
+        import("package.tools.cmake").install(package, configs)
+        
+        if package:config("standalone") then
+            os.mv("$(package:installdir())/lib/cmake/Champollion", "$(package:installdir())/lib/cmake/ChampollionStatic")
+        end
+
+        os.cp("$(package:installdir())/LICENSE", "$(package:installdir())/share/Champollion/copyright")
+    end)
+    
+    on_test(function(package)
+        -- 
+    end)
