7454852c4bfa836306f012c68487f96a46a2843b
Mrowr Purr <mrowr.purr@gmail.com>
2023-10-18 13:07:39 -0700

    Always static library

--- packages/c/champollion/xmake.lua
+++ packages/c/champollion/xmake.lua
@@ -2,24 +2,6 @@ package("champollion")
     set_homepage("https://github.com/Orvid/Champollion")
     set_description("A description for Champollion")
     add_urls("https://github.com/Orvid/Champollion.git")
-    
-    add_configs("standalone", { description = "Compile as a static library.", default = false, type = "boolean"})
-
     on_install(function(package)
-        local configs = {}
-        if package:config("standalone") then
-            table.insert(configs, "-DCHAMPOLLION_STATIC_LIBRARY=ON")
-        else
-            table.insert(configs, "-DCHAMPOLLION_STATIC_LIBRARY=OFF")
-        end
-        
-        import("package.tools.cmake").install(package, configs)
-        
-        if package:config("standalone") then
-            os.mv("$(package:installdir())/lib/cmake/Champollion", "$(package:installdir())/lib/cmake/ChampollionStatic")
-        end
-    end)
-    
-    on_test(function(package)
-        -- 
+        import("package.tools.cmake").install(package, {"-DCHAMPOLLION_STATIC_LIBRARY=ON"})
     end)
