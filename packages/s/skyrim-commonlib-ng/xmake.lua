285604cc6892696c679d72a7d4767b184e132c45
Mrowr Purr <mrowr.purr@gmail.com>
2025-02-17 15:25:49 -0800

    Fix spdlog dependency

--- packages/s/skyrim-commonlib-ng/xmake.lua
+++ packages/s/skyrim-commonlib-ng/xmake.lua
@@ -17,8 +17,7 @@ package("skyrim-commonlib-ng")
     add_configs("vr", {description = "Enable runtime support for Skyrim VR", default = true, type = "boolean"})
 
     add_deps("skse_plugin_info", "rapidcsv")
-    add_deps("fmt", { configs = { header_only = false } })
-    add_deps("spdlog", { configs = { header_only = false, fmt_external = true } })
+    add_deps("spdlog", { configs = { header_only = false, wchar = true, std_format = true } })
 
     add_syslinks("version", "user32", "shell32", "ole32", "advapi32")
 
