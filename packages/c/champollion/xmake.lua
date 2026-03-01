36aca18e1486f45b185ba11e576239870045ef49
Mrowr Purr <mrowr.purr@gmail.com>
2026-03-01 03:12:52 -0800

    Fix champollion config

--- packages/c/champollion/xmake.lua
+++ packages/c/champollion/xmake.lua
@@ -4,11 +4,15 @@ package("champollion")
     add_urls("https://github.com/mrowrpurr/Champollion.git")
     add_versions("mrowrpurr", "mrowrpurr")
     on_install(function(package)
-        local configs = {"-DCHAMPOLLION_STATIC_LIBRARY=ON"}
+        local configs = {
+            "-DCHAMPOLLION_STATIC_LIBRARY=ON",
+            "-DCMAKE_TOOLCHAIN_FILE=",
+            "-DVCPKG_MANIFEST_MODE=OFF",
+        }
         if package:debug() then
             table.insert(configs, "-DCMAKE_BUILD_TYPE=Debug")
         else
             table.insert(configs, "-DCMAKE_BUILD_TYPE=Release")
         end
-        import("package.tools.cmake").install(package, configs)
+        import("package.tools.cmake").install(package, configs, {envs = {VCPKG_ROOT = ""}})
     end)
