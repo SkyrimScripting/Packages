7876c187aedcece5bccbb3649dd6060343df43eb
Mrowr Purr <mrowr.purr@gmail.com>
2026-03-01 03:07:25 -0800

    Use champollion fork (bug fixes)

--- packages/c/champollion/xmake.lua
+++ packages/c/champollion/xmake.lua
@@ -1,15 +1,14 @@
 package("champollion")
-    set_homepage("https://github.com/Orvid/Champollion")
-    set_description("A description for Champollion")
-    add_urls("https://github.com/Orvid/Champollion.git")
+    set_homepage("https://github.com/mrowrpurr/Champollion")
+    set_description("A PEX to Papyrus Decompiler for Skyrim, Fallout 4 and Starfield")
+    add_urls("https://github.com/mrowrpurr/Champollion.git")
+    add_versions("mrowrpurr", "mrowrpurr")
     on_install(function(package)
         local configs = {"-DCHAMPOLLION_STATIC_LIBRARY=ON"}
-    
         if package:debug() then
             table.insert(configs, "-DCMAKE_BUILD_TYPE=Debug")
         else
             table.insert(configs, "-DCMAKE_BUILD_TYPE=Release")
         end
-
         import("package.tools.cmake").install(package, configs)
     end)
