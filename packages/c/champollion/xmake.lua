b586240082a16facb3679b2e0af0bf304306c9e6
Mrowr Purr <mrowr.purr@gmail.com>
2026-03-02 15:40:03 -0800

    Build Champollion using xmake

--- packages/c/champollion/xmake.lua
+++ packages/c/champollion/xmake.lua
@@ -3,12 +3,20 @@ package("champollion")
     set_description("A PEX to Papyrus Decompiler for Skyrim, Fallout 4 and Starfield")
     add_urls("https://github.com/SkyrimScriptingBeta/Champollion.git")
     add_versions("mrowrpurr", "mrowrpurr")
-    on_install(function(package)
-        local configs = {"-DCHAMPOLLION_STATIC_LIBRARY=ON"}
-        if package:debug() then
-            table.insert(configs, "-DCMAKE_BUILD_TYPE=Debug")
-        else
-            table.insert(configs, "-DCMAKE_BUILD_TYPE=Release")
+
+    add_configs("kind", {description = "Library type (static or shared)", default = "static", values = {"static", "shared"}})
+    add_configs("build_exe", {description = "Build the Champollion command-line executable", default = false, type = "boolean"})
+
+    on_load(function(package)
+        if package:config("build_exe") then
+            package:add("deps", "boost", {configs = {program_options = true}})
+            package:add("deps", "fmt")
         end
-        import("package.tools.cmake").install(package, configs)
+    end)
+
+    on_install(function(package)
+        local configs = {}
+        configs.kind = package:config("kind")
+        configs.build_exe = package:config("build_exe")
+        import("package.tools.xmake").install(package, configs)
     end)
